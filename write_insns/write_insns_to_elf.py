0bfd2ff33dacbbc07a1d35ad5eea1194a65c2240
Mike Wong <mdw362@nyu.edu>
2020-11-07 10:04:04 -0500

    Update order of section writing, include elf header

--- write_insns/write_insns_to_elf.py
+++ write_insns/write_insns_to_elf.py
@@ -1,27 +1,59 @@
 
-# disas1.py
 
 from elftools.elf.elffile import ELFFile
-from capstone import *
+import array
+import sys
 
-def read_elf():
+sections_to_extract = ['socket2', 'maps', 'license', '.eh_frame', '.symtab', '.relsocket2', '.rel.eh_frame', '.strtab']
+def read_elf_sections():
+    offset = 0
     with open('./sockex2_kern.o', 'rb') as f:
-        e = ELFFile(f)
-        for section in e.iter_sections():
-            print('SECTION: ', section.name)
+        f2 = open('./test_bytes.o', 'ab')
+        elf = ELFFile(f)
+        print('num sections: ' , elf.num_sections())
+        for section_name in sections_to_extract:
+            print('Getting ', section_name)
+            section = elf.get_section_by_name(section_name) 
             ops = section.data()
+            byte_array = bytearray(ops)
+            f2.write(byte_array)
             print(ops)
+            print (hex(section['sh_addr']), section.name, section['sh_size'])
+            offset += section['sh_size']
 
-def write_to_file():
-    byteList = [0xb4, 0x16, 6, 2, 0x00]
-    byteArray = bytearray(byteList)
-    with open('test_bytes.o', 'wb') as f:
-        f.write(byteArray)     
+        print('elf header: ', elf._parse_elf_header())
+
+        print('Offset = ', offset)
+#        byte = f.read(1)
+#        index = 0
+#        while byte != b"":
+#            byte = f.read(1)
+##            f2.write(byte)
+#            index += 1
+
+
+#Read first 64 bytes of the ELF which contains the
+#ELF file header (magic number, format, version, etc.)
+def read_elf_file_header():
+
+    byte_list = []
+
+    with open("test_bytes.o", "wb") as f2:
+        f2.write(b"")
+
+    with open("sockex2_kern.o", "rb") as f:
+        byte = 0x0
+        index = 0
+        while byte != b"" and index < 64:
+            with open("test_bytes.o", "ab") as f2:
+                byte = f.read(1)
+                f2.write(byte)
+            index += 1
 
 def main():
    
-#    write_to_file() 
-    read_elf()
+    read_elf_file_header()
+    read_elf_sections()
 
 if __name__ == '__main__':
     main()
